e9e65acb98c1bb6953b8f9ad024561efb96cce46
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-06-26 18:47:01 +0200

    dodanie projektu

---
+++ jaz-s24967-nbp (2)/jaz-s24967-nbp/src/main/java/com/example/jazs24967nbp/JazS24967NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs24967nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24967NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24967NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s24967-nbp (2)/jaz-s24967-nbp/src/test/java/com/example/jazs24967nbp/JazS24967NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs24967nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24967NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
